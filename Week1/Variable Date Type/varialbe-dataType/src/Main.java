8671f119e4e3d178124ec4b793ec3f58739dbefc
DanggHuyy99 <nguyendanghuyy99@gmail.com>
2023-04-07 16:41:00 +0700

    fix

---
+++ Week1/Variable Date Type/varialbe-dataType/src/Main.java
@@ -0,0 +1,18 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+        float u = 14.5f;
+        double y = 20.5;
+        boolean loveYou = true;
+        char name = 'H';
+        String myName = "Đăng Huy";
+        System.out.println("i = " + i);
+        System.out.println("u = " + u);
+        System.out.println("y = " + y);
+        System.out.println("loveYou = " + loveYou);
+        System.out.println("char = " + name);
+        System.out.println("My name is: " + myName);
+    }
+}
