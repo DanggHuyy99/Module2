6d0d93cd7dd53c8d00a37b57bd16872642745e34
DanggHuyy99 <nguyendanghuyy99@gmail.com>
2023-04-09 08:48:49 +0700

    add file day of month

---
+++ Week1/tesst.js
@@ -0,0 +1,9 @@
+function oddNumbers() {
+    var arr = [];
+    //your code here 
+    for (let i = 1; i < 50; i = i+ 2){
+        arr.push(i)
+    }
+    return console.log(arr) 
+}
+a = oddNumbers();
