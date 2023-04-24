a626bf9836f5ff3880d4ef35bcd61663c7f28d4d
DanggHuyy99 <nguyendanghuyy99@gmail.com>
2023-04-24 14:42:35 +0700

    add file refactoring

---
+++ Week3/CleanCode/CleanCode/src/refactoring/Calculator.java
@@ -0,0 +1,29 @@
+package refactoring;
+
+public class Calculator {
+    int soDauTien;
+    int soThuHai;
+    char toanTu;
+    public Calculator(){};
+    public static final char ADDITION = '+';
+    public static final char SUBTRACTION = '-';
+    public static final char MULTIPLICATION = '*';
+    public static final char DIVISION = '/';
+    public static int calculator(int soDauTien, int soThuHai, char toanTu) {
+        switch (toanTu) {
+            case ADDITION:
+                return soDauTien + soThuHai;
+            case SUBTRACTION:
+                return soDauTien - soThuHai;
+            case MULTIPLICATION:
+                return soDauTien * soThuHai;
+            case DIVISION:
+                if (soThuHai != 0)
+                    return soDauTien / soThuHai;
+                else
+                    throw new RuntimeException("Can not divide by 0");
+            default:
+                throw new RuntimeException("Unsupported operation");
+        }
+    }
+}
---
+++ Week3/CleanCode/CleanCode/src/refactoring/Main.java
@@ -0,0 +1,19 @@
+package refactoring;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập số thứ 1:");
+        int firstOpenhand = scanner.nextInt();
+        System.out.println("Nhập số thứ 2:");
+        int secondOpenhand = scanner.nextInt();
+        System.out.println("Nhập biểu thức:");
+        char operator = scanner.next().charAt(0);
+
+        Calculator cal = new Calculator();
+
+        System.out.println(cal.calculator(firstOpenhand,secondOpenhand,operator));
+    }
+}
